3f2b895648fae6453b077d5cd49a72dd5a6d07af
Syafa2 <obbesp62@gmail.com>
2023-01-23 06:21:48 +0700

    Dhan8

---
+++ src/message/config.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+//apikey
+// lolkey ( lolhuman : https://api.lolhuman.xyz )
+global.lolkey = '809664a7101247b98fe5f332'
+
+global.mode = false
+global.autodltt = false
+global.counttroli = 555999777
+global.typemenu = 'document'
+global.owner = ['6285852688728'] //Masukin nomor mu
+global.ownernomer = "6285852688728" //Masukin nomor mu
+global.premium = ['6285852688728'] //Masukin nomor mu
+global.ownername = 'Ftn' //Masukin nama bot kamu
+global.botname = 'Ftn-Botz' //Masukin nama bot kamu
+global.fakename = 'Otong' //Masukin nama mu
+global.fakebody = 'Hubungi jika ingin sewabot' // Monggo kalau mau ganti
+global.packname = 'Sticker By' //Gakusah di ubah kalau mau di ubah Monggo 
+global.author = 'Fthn?' //Monggo kalau mau ubah
+global.sessionName = 'KamuNanya' //Gak usah di ubah
+global.jumlha = '999' //Gak usah di ubah
+global.jumhal = '100000000000000' //Gak usah di ubah
+global.jumlah = '1000000000' //Gak usah di ubah
+global.prefa = ['','!','.','#','&'] //Gak usah di ubah
+global.scmu = 'Sc?\nChat owner' //Monggo kalau mau ubah
+global.githubmu = 'https://github.com/Syafa2' //Isi nama github mu
+global.ytmu = 'https://youtube.com/channel/UCcnWMxH0nUbArCCkqubUeHA' //Gak usah di ubah
+global.ytvmu = 'https://youtube.com/shorts/b_L5jlwrFRs?feature=share' //Gak usah di ubah
+global.linkbebas = 'https://github.com/Syafa2' //Terserah mau luh isi web apa mau github atau link group juga bisa
+global.imglink = 'https://telegra.ph/file/b5ac5b79fd044eb90bfd9.jpg' // isi dengan image link/foto link
+global.teksqr = {
+gopay: '_Ini kak Qr Gopay nya_',
+dana: '_Ini kak Qr Dana nya_',
+ovo: '_Ini kak Qr Ovo nya_',
+}
+global.mess = {
+    success: '✅Done',
+    admin: '_Fitur Hanya Dapat Di Gunakan Oleh Admin Group!_',
+    botAdmin: '_Bot Harus Menjadi Admin Terdahulu!_',
+    premium: '_Fitur Hanya Dapat Di Gunakan User Premium!_',
+    ban: '_Kamu Telah Di Banned Oleh Bot, Silakan Hubungi Owner Untuk Di Buka!_',
+    owner: '_Fitur Hanya Dapat Di Gunakan Oleh Owner Bot!_',
+    group: '_Fitur Hanya Dapat Di Gunakan Di Dalam Group!_',
+    private: '_Fitur Hanya Dapat Di Gunakan Di Chat Pribadi!_',
+    bot: '_Fitur Hanya Dapat Di Gunakan User Bot!_',
+    wait: '_Loading . . ._',
+    errapi: '_Error Mungkin Apikey Tidak Valid!_',
+    errmor: '_Error Kesalahan Sistem_',
+}
+
+global.fake = fs.readFileSync('./src/media/image/fake.jpg')
+global.thumb = fs.readFileSync('./src/media/image/thumb.jpg')
+global.fakevideo = fs.readFileSync('./src/media/video/fakevideo.mp4')
+
+global.apidhaniofc = `https://api.dhaniofc.my.id`
+global.dhakey = `exptgl30`
+
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
